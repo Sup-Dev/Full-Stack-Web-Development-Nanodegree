7a62ba501c09142da483f5374e8836ff500d6984
Sup-Dev <sourabh.dev90@gmail.com>
2015-09-21 11:31:57 +0530

    added more movie to catalog

--- 1 - Programming Foundations with Python/Lesson 3/entertainment_center.py
+++ 1 - Programming Foundations with Python/Lesson 3/entertainment_center.py
@@ -12,5 +12,25 @@ avatar = media.Movie("Avatar",
 					"https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
 					"https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-print(avatar.storyline)					
-avatar.show_trailer()
+#print(avatar.storyline)					
+#avatar.show_trailer()
+
+school_of_rock = media.Movie("School of Rock",
+							"Using rock music to learn",
+							"https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+							"https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille",
+						"A rat is a chef in Paris",
+						"https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+						"https://www.youtube.com/watch?v=c3sBBRxDAqk")			
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+								"Storyline",
+								"https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+								"https://www.youtube.com/watch?v=BYRWfS2s2v4")
+
+hunger_games = media.Movie("Hunger Games", 
+							"Storyline",
+							"https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+							"https://www.youtube.com/watch?v=4S9a5V9ODuY")
