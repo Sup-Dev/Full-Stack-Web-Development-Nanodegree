65bac8ea6211978e82774f1be4e3d3e8ee11f3d2
Sup-Dev <sourabh.dev90@gmail.com>
2015-09-21 11:38:36 +0530

    linked entertainment_center.py to fresh_tomatoes.py to spawn the website

--- 1 - Programming Foundations with Python/Lesson 3/entertainment_center.py
+++ 1 - Programming Foundations with Python/Lesson 3/entertainment_center.py
@@ -1,9 +1,10 @@
+import fresh_tomatoes
 import media
 
 toy_story = media.Movie("Toy Story", 
 						"A story of a boy and his toys that come to life",
 						"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-						"https://www.youtube.com/watch?v=vwyZH85NQC4")
+						"https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 #print(toy_story.storyline)						
 
@@ -34,3 +35,6 @@ hunger_games = media.Movie("Hunger Games",
 							"Storyline",
 							"https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
 							"https://www.youtube.com/watch?v=4S9a5V9ODuY")
+
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+fresh_tomatoes.open_movies_page(movies)
