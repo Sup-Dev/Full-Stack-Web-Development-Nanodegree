894ecb6b232fba3d62002d589da5b0414121deaa
Sup-Dev <sourabh.dev90@gmail.com>
2015-09-22 10:38:49 +0530

    made changes to show more movie related information

--- 2 - P1 Movie Website Trailer/entertainment_center.py
+++ 2 - P1 Movie Website Trailer/entertainment_center.py
@@ -1,42 +1,66 @@
 import fresh_tomatoes
 import media
 
-toy_story = media.Movie("Toy Story", 
-						"A story of a boy and his toys that come to life",
-						"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-						"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+wall_e = media.Movie("Wall-E", 
+					"In the distant future, a small waste collecting robot inadvertently embarks on a space journey that will ultimately decide the fate of mankind.",
+					98,
+					"27 June 2008",
+					"Andrew Stanton",
+					"G",
+					"https://upload.wikimedia.org/wikipedia/en/c/c2/WALL-Eposter.jpg",
+					"https://www.youtube.com/watch?v=ZisWjdjs-gM")
 
 #print(toy_story.storyline)						
 
 avatar = media.Movie("Avatar",
 					"A marine on an alien planet",
+					162,
+					"18 December 2009",
+					"James Cameron",
+					"PG-13",
 					"https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
 					"https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 #print(avatar.storyline)					
 #avatar.show_trailer()
 
-school_of_rock = media.Movie("School of Rock",
-							"Using rock music to learn",
-							"https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-							"https://www.youtube.com/watch?v=3PsUJFEBC74")
+fight_club = media.Movie("Fight Club",
+						"An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more",
+						139,
+						"15 October 1999",
+						"David Fincher",
+						"R",
+						"https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",
+						"https://www.youtube.com/watch?v=BdJKm16Co6M")
 
 ratatouille = media.Movie("Ratatouille",
 						"A rat is a chef in Paris",
+						111,
+						"29 June 2007",
+						"Brad Bird, Jan Pinkava",
+						"G",
 						"https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
 						"https://www.youtube.com/watch?v=c3sBBRxDAqk")			
 
-midnight_in_paris = media.Movie("Midnight in Paris",
-								"Storyline",
-								"https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-								"https://www.youtube.com/watch?v=BYRWfS2s2v4")
+george_of_the_jungle = media.Movie("George of the Jungle",
+								"George grows up in the jungle raised by apes. Based on the Cartoon series.",
+								92,
+								"16 July 1997",
+								"Sam Weisman",
+								"PG",
+								"https://upload.wikimedia.org/wikipedia/en/f/f8/George_Of_The_Jungle.jpg",
+								"https://www.youtube.com/watch?v=opqC7DhdaTM")
 
-hunger_games = media.Movie("Hunger Games", 
-							"Storyline",
-							"https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-							"https://www.youtube.com/watch?v=4S9a5V9ODuY")
+the_dark_knight = media.Movie("The Dark Knight", 
+							"When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+							152,
+							"18 July 2008",
+							"Christopher Nolan",
+							"PG-13",
+							"https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
+							"https://www.youtube.com/watch?v=EXeTwQWrcwY")
 
-movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
-#fresh_tomatoes.open_movies_page(movies)
+movies = [wall_e, avatar, fight_club, ratatouille, george_of_the_jungle, the_dark_knight]
+fresh_tomatoes.open_movies_page(movies)
 #print(media.Movie.VALID_RATINGS)
 print(media.Movie.__doc__)
--- 2 - P1 Movie Website Trailer/media.py
+++ 2 - P1 Movie Website Trailer/media.py
@@ -5,9 +5,13 @@ class Movie():
 
 	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+	def __init__(self, movie_title, movie_storyline, movie_duration, movie_release_date, movie_director, movie_rating, poster_image, trailer_youtube):
 		self.title = movie_title
 		self.storyline = movie_storyline
+		self.duration = movie_duration
+		self.release = movie_release_date
+		self.director = movie_director
+		self.rating = movie_rating
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 
