bcc6fa0f320f60448862c78a952fdc3297ba09a8
Sup-Dev <sourabh.dev90@gmail.com>
2015-12-10 07:55:21 +0530

    added greet by period endpoint

--- 6 - Developing Scalable Apps in Python /Lesson 2/000_Hello_Endpoints/helloworld_api.py
+++ 6 - Developing Scalable Apps in Python /Lesson 2/000_Hello_Endpoints/helloworld_api.py
@@ -18,6 +18,12 @@ REQUEST_CONTAINER = endpoints.ResourceContainer(
     name=messages.StringField(1),
 )
 
+REQUEST_CONTAINER_PERIOD = endpoints.ResourceContainer(
+    message_types.VoidMessage,
+    name=messages.StringField(1),
+    period=messages.StringField(2),
+)
+
 
 package = 'Hello'
 
@@ -40,6 +46,12 @@ class HelloWorldApi(remote.Service):
       path = "sayHelloByName", http_method='GET', name = "sayHelloByName")
     def say_hello_by_name(self, request):
       greet = "Hello {}".format(request.name)
+      return Hello(greeting=greet)
+
+    @endpoints.method(REQUEST_CONTAINER_PERIOD, Hello,
+      path = "greetByPeriod", http_method='GET', name = "greetByPeriod")
+    def greet_by_period(self, request):
+      greet = "Good {1} {0}!".format(request.name, request.period)
       return Hello(greeting=greet)
 
 
