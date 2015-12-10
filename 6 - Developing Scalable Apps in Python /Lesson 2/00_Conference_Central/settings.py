b9b9b652c91bf7f58ccf1162e0d2385e2e6716d7
Sup-Dev <sourabh.dev90@gmail.com>
2015-12-10 08:39:54 +0530

    updated web client id

--- 6 - Developing Scalable Apps in Python /Lesson 2/00_Conference_Central/settings.py
+++ 6 - Developing Scalable Apps in Python /Lesson 2/00_Conference_Central/settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '862296972681-6hkmamokv7ig49puefrv0v1e64bpbtar.apps.googleusercontent.com'
 
