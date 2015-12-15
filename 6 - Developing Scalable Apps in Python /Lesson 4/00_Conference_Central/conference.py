0423505f03518ebe358cf672de35b56802726e68
Sup-Dev <sourabh.dev90@gmail.com>
2015-12-15 09:49:03 +0530

    app now shows all conference

--- 6 - Developing Scalable Apps in Python /Lesson 4/00_Conference_Central/conference.py
+++ 6 - Developing Scalable Apps in Python /Lesson 4/00_Conference_Central/conference.py
@@ -26,8 +26,12 @@ from models import Profile
 from models import ProfileMiniForm
 from models import ProfileForm
 from models import TeeShirtSize
+
 from models import Conference
 from models import ConferenceForm
+from models import ConferenceForms
+from models import ConferenceQueryForm
+from models import ConferenceQueryForms
 
 from utils import getUserId
 
@@ -203,6 +207,17 @@ class ConferenceApi(remote.Service):
     def createConference(self, request):
         """Create new conference."""
         return self._createConferenceObject(request)
+
+    @endpoints.method(ConferenceQueryForms, ConferenceForms, path='queryConferences',
+        http_method='POST', name='queryConferences')
+    def queryConferences(self, request):
+        """Query for conferences."""
+        conferences = Conference.query()
+
+         # return individual ConferenceForm object per Conference
+        return ConferenceForms(
+            items=[self._copyConferenceToForm(conf, "") for conf in conferences]
+        )
 
 # registers API
 api = endpoints.api_server([ConferenceApi]) 
